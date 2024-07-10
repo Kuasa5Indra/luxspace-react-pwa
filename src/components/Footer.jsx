ec0eab8973b9f252efa6a17a8c5810e9c65bd206
Kuasa5Indra <adityaindra135@gmail.com>
2024-07-10 10:01:41 +0700

    feat : adding react-router-dom

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 function Footer() {
+    const [year, setYear] = useState(new Date().getFullYear());
+
+    useEffect(() => {
+        // Update the year when the component mounts
+        setYear(new Date().getFullYear());
+    }, []);
+
     return (
         <footer className="flex text-center px-4 py-8 justify-center">
             <p className="text-sm">
-                Copyright 2022 . Allrights reserved by LuxSpace
+                Copyright {year} . Allrights reserved by LuxSpace
             </p>
         </footer>
     )
